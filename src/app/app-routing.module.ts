1006453b5988762dde24157040ca7c2048b73160
josgro <josgrooten@gmail.com>
2022-04-04 10:23:04 +0200

    added routing to most links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'shop', component: ShopComponent},
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
